0e187d75b9565ec249fb657a393553415922ae0a
ngbesso <ngbessoalainserge@gmail.com>
2026-01-09 22:27:35 -0500

    feat(usecases): add MarkMessageAsRead use case

--- app/UseCases/Contact/MarkMessageAsRead.php
+++ app/UseCases/Contact/MarkMessageAsRead.php
@@ -2,7 +2,38 @@
 
 namespace App\UseCases\Contact;
 
+use App\Core\Entities\Contact;
+use App\Core\Repositories\ContactRepositoryInterface;
+use App\Core\Exceptions\ContactNotFoundException;
+
+/**
+ * Use Case : Marquer un message comme lu
+ *
+ * @package App\UseCases\Contact
+ */
 class MarkMessageAsRead
 {
+    public function __construct(
+        private readonly ContactRepositoryInterface $contactRepository
+    ) {}
 
+    /**
+     * Marquer comme lu
+     *
+     * @param int $contactId
+     * @return Contact
+     * @throws ContactNotFoundException
+     */
+    public function execute(int $contactId): Contact
+    {
+        $contact = $this->contactRepository->findById($contactId);
+
+        if ($contact === null) {
+            throw ContactNotFoundException::withId($contactId);
+        }
+
+        $contact->markAsRead();
+
+        return $this->contactRepository->update($contact);
+    }
 }

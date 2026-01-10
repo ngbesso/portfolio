c16edc83619eeabb6cfaba425b1d86d91c7c19f4
ngbesso <ngbessoalainserge@gmail.com>
2026-01-09 22:27:34 -0500

    feat(usecases): add ArchiveProject use case

--- app/UseCases/Project/ArchiveProject.php
+++ app/UseCases/Project/ArchiveProject.php
@@ -2,7 +2,40 @@
 
 namespace App\UseCases\Project;
 
+use App\Core\Entities\Project;
+use App\Core\Repositories\ProjectRepositoryInterface;
+use App\Core\Exceptions\ProjectNotFoundException;
+
+/**
+ * Use Case : Archiver un projet
+ *
+ * Permet de retirer un projet de la vue publique sans le supprimer
+ *
+ * @package App\UseCases\Project
+ */
 class ArchiveProject
 {
+    public function __construct(
+        private readonly ProjectRepositoryInterface $projectRepository
+    ) {}
 
+    /**
+     * Archiver un projet
+     *
+     * @param int $projectId
+     * @return Project
+     * @throws ProjectNotFoundException
+     */
+    public function execute(int $projectId): Project
+    {
+        $project = $this->projectRepository->findById($projectId);
+
+        if ($project === null) {
+            throw ProjectNotFoundException::withId($projectId);
+        }
+
+        $project->archive();
+
+        return $this->projectRepository->update($project);
+    }
 }

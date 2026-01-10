434ae4c0fe10728f1cd26cbdaafdec97d4c885a8
ngbesso <ngbessoalainserge@gmail.com>
2026-01-09 22:27:33 -0500

    feat(usecases): add GetProjectById use case

--- app/UseCases/Project/GetProjectById.php
+++ app/UseCases/Project/GetProjectById.php
@@ -2,7 +2,37 @@
 
 namespace App\UseCases\Project;
 
+use App\Core\Entities\Project;
+use App\Core\Repositories\ProjectRepositoryInterface;
+use App\Core\Exceptions\ProjectNotFoundException;
+
+/**
+ * Use Case : Récupérer un projet par son ID
+ *
+ * @package App\UseCases\Project
+ */
 class GetProjectById
 {
+    public function __construct(
+        private readonly ProjectRepositoryInterface $projectRepository
+    ) {}
 
+    /**
+     * Exécuter la récupération
+     *
+     * @param int $projectId
+     * @return Project
+     * @throws ProjectNotFoundException
+     */
+    public function execute(int $projectId): Project
+    {
+        $project = $this->projectRepository->findById($projectId);
+
+        if ($project === null) {
+            throw ProjectNotFoundException::withId($projectId);
+        }
+
+        return $project;
+    }
 }
+

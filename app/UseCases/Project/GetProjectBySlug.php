c9c1f143bb58874d1ce8126552eb401cb46744a3
ngbesso <ngbessoalainserge@gmail.com>
2026-01-09 22:27:33 -0500

    feat(usecases): add GetProjectBySlug use case

--- app/UseCases/Project/GetProjectBySlug.php
+++ app/UseCases/Project/GetProjectBySlug.php
@@ -2,7 +2,43 @@
 
 namespace App\UseCases\Project;
 
+use App\Core\Entities\Project;
+use App\Core\Repositories\ProjectRepositoryInterface;
+use App\Core\Exceptions\ProjectNotFoundException;
+
+/**
+ * Use Case : Récupérer un projet par son slug
+ * Utilisé pour les pages publiques avec URLs SEO-friendly
+ *
+ * @package App\UseCases\Project
+ */
 class GetProjectBySlug
 {
+    public function __construct(
+        private readonly ProjectRepositoryInterface $projectRepository
+    ) {}
 
+    /**
+     * Exécuter la récupération
+     *
+     * @param string $slug
+     * @return Project
+     * @throws ProjectNotFoundException
+     */
+    public function execute(string $slug): Project
+    {
+        $project = $this->projectRepository->findBySlug($slug);
+
+        if ($project === null) {
+            throw ProjectNotFoundException::withSlug($slug);
+        }
+
+        // Règle métier : Seuls les projets publiés sont accessibles par slug
+        // Les brouillons et archivés ne doivent pas être visibles
+        if (!$project->isPublished()) {
+            throw ProjectNotFoundException::withSlug($slug);
+        }
+
+        return $project;
+    }
 }

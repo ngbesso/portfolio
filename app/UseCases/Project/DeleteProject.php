0868e1f2f4542be1dd7e65ba1c196c7c7c3f0b31
ngbesso <ngbessoalainserge@gmail.com>
2026-01-09 22:27:33 -0500

    feat(usecases): add DeleteProject use case

--- app/UseCases/Project/DeleteProject.php
+++ app/UseCases/Project/DeleteProject.php
@@ -2,7 +2,44 @@
 
 namespace App\UseCases\Project;
 
+use App\Core\Repositories\ProjectRepositoryInterface;
+use App\Core\Exceptions\ProjectNotFoundException;
+
+/**
+ * Use Case : Supprimer un projet
+ *
+ * @package App\UseCases\Project
+ */
 class DeleteProject
 {
+    public function __construct(
+        private readonly ProjectRepositoryInterface $projectRepository
+    ) {}
 
+    /**
+     * Exécuter la suppression
+     *
+     * @param int $projectId
+     * @return bool
+     * @throws ProjectNotFoundException
+     */
+    public function execute(int $projectId): bool
+    {
+        // 1. Vérifier que le projet existe
+        $project = $this->projectRepository->findById($projectId);
+
+        if ($project === null) {
+            throw ProjectNotFoundException::withId($projectId);
+        }
+
+        // NOTE : Le controller ou un service devrait supprimer l'image
+        // associée avant d'appeler ce use case
+        // Ex: if ($project->getImage()) { Storage::delete($project->getImage()); }
+
+        // 2. Supprimer le projet
+        $deleted = $this->projectRepository->delete($projectId);
+
+        return $deleted;
+    }
 }
+

1bdd136b59024fd57573f0fa59044affe66909d4
ngbesso <ngbessoalainserge@gmail.com>
2026-01-17 12:23:26 -0500

    feat(presentation): add frontend routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,30 @@
 <?php
 
+use App\Presentation\Http\Controllers\ContactController;
+use App\Presentation\Http\Controllers\HomeController;
+use App\Presentation\Http\Controllers\ProjectController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/*
+|--------------------------------------------------------------------------
+| Routes Frontend (Public)
+|--------------------------------------------------------------------------
+*/
+
+// Page d'accueil
+Route::get('/', [HomeController::class, 'index'])
+    ->name('home');
+
+// Pages Projets
+Route::get('/projects', [ProjectController::class, 'index'])
+    ->name('projects.index');
+
+Route::get('/projects/{slug}', [ProjectController::class, 'show'])
+    ->name('projects.show');
+
+// Formulaire de contact
+Route::get('/contact', [ContactController::class, 'show'])
+    ->name('contact.show');
+
+Route::post('/contact', [ContactController::class, 'store'])
+    ->name('contact.store');
